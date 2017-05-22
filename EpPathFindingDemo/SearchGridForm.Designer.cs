9149708a6199ab1883d24e03f2d18996f027964d
Woong Gyu La <juhgiyo@gmail.com>
2017-05-23 04:31:41 +0900

    UI updated

--- EpPathFindingDemo/SearchGridForm.Designer.cs
+++ EpPathFindingDemo/SearchGridForm.Designer.cs
@@ -33,6 +33,7 @@
             this.btnClearWall = new System.Windows.Forms.Button();
             this.cbUseRecursive = new System.Windows.Forms.CheckBox();
             this.cbbJumpType = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnSearch
@@ -68,7 +69,7 @@
             // cbUseRecursive
             // 
             this.cbUseRecursive.AutoSize = true;
-            this.cbUseRecursive.Location = new System.Drawing.Point(471, 16);
+            this.cbUseRecursive.Location = new System.Drawing.Point(533, 17);
             this.cbUseRecursive.Name = "cbUseRecursive";
             this.cbUseRecursive.Size = new System.Drawing.Size(96, 17);
             this.cbUseRecursive.TabIndex = 6;
@@ -79,10 +80,19 @@
             // 
             this.cbbJumpType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbbJumpType.FormattingEnabled = true;
-            this.cbbJumpType.Location = new System.Drawing.Point(248, 12);
+            this.cbbJumpType.Location = new System.Drawing.Point(338, 16);
             this.cbbJumpType.Name = "cbbJumpType";
             this.cbbJumpType.Size = new System.Drawing.Size(189, 21);
             this.cbbJumpType.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(247, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Diagonal Mode: ";
             // 
             // SearchGridForm
             // 
@@ -90,6 +100,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.ClientSize = new System.Drawing.Size(693, 284);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.cbbJumpType);
             this.Controls.Add(this.cbUseRecursive);
             this.Controls.Add(this.btnClearWall);
@@ -114,6 +125,7 @@
         private System.Windows.Forms.Button btnClearWall;
         private System.Windows.Forms.CheckBox cbUseRecursive;
         private System.Windows.Forms.ComboBox cbbJumpType;
+        private System.Windows.Forms.Label label1;
     }
 }
 
